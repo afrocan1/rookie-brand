8b2f6ab45c06e8a48660b066dffb78c131dde0f9
afrocan1 <114925535+afrocan1@users.noreply.github.com>
2026-05-13 16:02:15 +0000

    Update cta-button.tsx

--- src/components/cta-button.tsx
+++ src/components/cta-button.tsx
@@ -1,15 +1,16 @@
 'use client'
-
-import { useRouter } from 'next/navigation'
 import { FiArrowRight } from 'react-icons/fi'
 
 export function CTAButton() {
-  const router = useRouter()
+  function handleClick() {
+    const navBtn = document.querySelector('header button[title="Get Started"]') as HTMLButtonElement
+    if (navBtn) navBtn.click()
+  }
 
   return (
     <div className="button-wrapper">
       <button
-        onClick={() => router.push('')}
+        onClick={handleClick}
         className="w-[calc(100%-2px)] h-[calc(100%-2px)] absolute z-30 top-[1px] left-[1px] text-sm bg-black flex items-center justify-center gap-5 rounded-3xl focus:outline-none"
       >
         Get Started
